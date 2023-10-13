fbfb1f0f3d52bde340d9b8d7aca15712d4276eb1
Sviatoslav Kondur <sviatikkondur5@gmail.com>
2023-10-13 11:45:58 +0200

    implemented sorting - all tests passed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/accessible-emoji */
 import React, { useState } from 'react';
 import cn from 'classnames';
@@ -47,16 +49,63 @@ const filterProducts = (
   return filteredProducts;
 };
 
+const SORT_BY_PRODUCT = 'product';
+const SORT_BY_ID = 'id';
+const SORT_BY_CATEGORY = 'category';
+const SORT_BY_USER = 'user';
+
+const sortProducts = (filteredProducts, isAscending, sortBy) => {
+  const sortedProducts = [...filteredProducts];
+
+  if (sortBy) {
+    switch (sortBy) {
+      case SORT_BY_ID:
+        sortedProducts.sort((a, b) => a.id - b.id);
+        break;
+
+      case SORT_BY_PRODUCT:
+        sortedProducts.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+
+      case SORT_BY_CATEGORY:
+        // eslint-disable-next-line
+        sortedProducts.sort((a, b) => a.category.title.localeCompare(b.category.title));
+        break;
+
+      case SORT_BY_USER:
+        sortedProducts.sort((a, b) => a.user.name.localeCompare(b.user.name));
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  if (!isAscending) {
+    return sortedProducts.reverse();
+  }
+
+  return sortedProducts;
+};
+
 export const App = () => {
   const [selectedUserId, setSelectedUser] = useState(0);
   const [query, setQuery] = useState('');
   const [selectedCategoryIDs, setSelectedCategoryIDs] = useState([]);
+  const [isAscending, setIsAscending] = useState(true);
+  const [sortBy, setSortBy] = useState('');
 
   const filteredProducts = filterProducts(
     products,
     selectedUserId,
     query,
     selectedCategoryIDs,
+  );
+
+  const sortedProducts = sortProducts(
+    filteredProducts,
+    isAscending,
+    sortBy,
   );
 
   return (
@@ -201,7 +250,28 @@ export const App = () => {
 
                         <a href="#/">
                           <span className="icon">
-                            <i data-cy="SortIcon" className="fas fa-sort" />
+                            <i
+                              data-cy="SortIcon"
+                              className={cn(
+                                'fas',
+                                { 'fa-sort': sortBy !== SORT_BY_ID },
+                                // eslint-disable-next-line
+                                { 'fa-sort-up': sortBy === SORT_BY_ID && isAscending },
+                                // eslint-disable-next-line
+                                { 'fa-sort-down': sortBy === SORT_BY_ID && !isAscending },
+                              )}
+                              onClick={() => {
+                                if (sortBy !== SORT_BY_ID) {
+                                  setSortBy(SORT_BY_ID);
+                                  setIsAscending(true);
+                                } else if (isAscending) {
+                                  setIsAscending(false);
+                                } else {
+                                  setSortBy('');
+                                  setIsAscending(true);
+                                }
+                              }}
+                            />
                           </span>
                         </a>
                       </span>
@@ -215,7 +285,25 @@ export const App = () => {
                           <span className="icon">
                             <i
                               data-cy="SortIcon"
-                              className="fas fa-sort-down"
+                              className={cn(
+                                'fas',
+                                { 'fa-sort': sortBy !== SORT_BY_PRODUCT },
+                                // eslint-disable-next-line
+                                { 'fa-sort-up': sortBy === SORT_BY_PRODUCT && isAscending },
+                                // eslint-disable-next-line
+                                { 'fa-sort-down': sortBy === SORT_BY_PRODUCT && !isAscending },
+                              )}
+                              onClick={() => {
+                                if (sortBy !== SORT_BY_PRODUCT) {
+                                  setSortBy(SORT_BY_PRODUCT);
+                                  setIsAscending(true);
+                                } else if (isAscending) {
+                                  setIsAscending(false);
+                                } else {
+                                  setSortBy('');
+                                  setIsAscending(true);
+                                }
+                              }}
                             />
                           </span>
                         </a>
@@ -228,7 +316,28 @@ export const App = () => {
 
                         <a href="#/">
                           <span className="icon">
-                            <i data-cy="SortIcon" className="fas fa-sort-up" />
+                            <i
+                              data-cy="SortIcon"
+                              className={cn(
+                                'fas',
+                                { 'fa-sort': sortBy !== SORT_BY_CATEGORY },
+                                // eslint-disable-next-line
+                                { 'fa-sort-up': sortBy === SORT_BY_CATEGORY && isAscending },
+                                // eslint-disable-next-line
+                                { 'fa-sort-down': sortBy === SORT_BY_CATEGORY && !isAscending },
+                              )}
+                              onClick={() => {
+                                if (sortBy !== SORT_BY_CATEGORY) {
+                                  setSortBy(SORT_BY_CATEGORY);
+                                  setIsAscending(true);
+                                } else if (isAscending) {
+                                  setIsAscending(false);
+                                } else {
+                                  setSortBy('');
+                                  setIsAscending(true);
+                                }
+                              }}
+                            />
                           </span>
                         </a>
                       </span>
@@ -240,7 +349,28 @@ export const App = () => {
 
                         <a href="#/">
                           <span className="icon">
-                            <i data-cy="SortIcon" className="fas fa-sort" />
+                            <i
+                              data-cy="SortIcon"
+                              className={cn(
+                                'fas',
+                                { 'fa-sort': sortBy !== SORT_BY_USER },
+                                // eslint-disable-next-line
+                                { 'fa-sort-up': sortBy === SORT_BY_USER && isAscending },
+                                // eslint-disable-next-line
+                                { 'fa-sort-down': sortBy === SORT_BY_USER && !isAscending },
+                              )}
+                              onClick={() => {
+                                if (sortBy !== SORT_BY_USER) {
+                                  setSortBy(SORT_BY_USER);
+                                  setIsAscending(true);
+                                } else if (isAscending) {
+                                  setIsAscending(false);
+                                } else {
+                                  setSortBy('');
+                                  setIsAscending(true);
+                                }
+                              }}
+                            />
                           </span>
                         </a>
                       </span>
@@ -249,7 +379,7 @@ export const App = () => {
                 </thead>
 
                 <tbody>
-                  {filteredProducts.map(product => (
+                  {sortedProducts.map(product => (
                     <tr data-cy="Product">
                       <td className="has-text-weight-bold" data-cy="ProductId">
                         {product.id}
